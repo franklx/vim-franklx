2b124820afc11aef254d0ac54631f06ae22e3f0f
Franco Lucchini <flucchini@gmail.com>
2020-11-14 08:10:25 +0100

    fixes

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
+
+# vim #
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 git clone https://github.com/franklx/vim-franklx ~/.vim/plugged/vim-franklx
+ln -s ~/.vim/plugged/vim-franklx/vimrc ~/.vimrc
 
-ln -s ~/.vimrc ~/.vim/plugged/vim-franklx/vimrc
-
-NVD="${XDG_DATA_HOME:-$HOME/.local/share}"
-NVC="${XDG_CONFIG_HOME:-$HOME/.config}"
-mkdir -p $NVD $NVC
-
-ln -s ~/.vim $NVC/nvim/init.vim
-ln -s ~/.vim/autoload $NVD/nvim/site/autoload
+# neovim #
+NVC="${XDG_CONFIG_HOME:-$HOME/.config}"/nvim
+NVD="${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site
+mkdir -p $NVC $NVD
+ln -s ~/.vimrc $NVC/init.vim
+ln -s ~/.vim/autoload $NVD/autoload
